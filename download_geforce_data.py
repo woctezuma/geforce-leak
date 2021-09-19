7c592d799d1509cfebbbaf20b6bb3ba0d03ae76d
Wok <woctezuma@users.noreply.github.com>
2021-09-19 10:08:33 +0200

    Override the official GeForce url with a back-up url

--- download_geforce_data.py
+++ download_geforce_data.py
@@ -1,9 +1,14 @@
 from src.disk_utils import save_to_disk
 from src.fetch_utils import fetch_all_pages, get_geforce_url
+from src.fetch_utils import get_geforce_backup_url
 
 
 def main():
     geforce_url = get_geforce_url()
+
+    # Override the URL, after the leak has been plugged, with a URL where back-up data is being hosted by the leaker.
+    # Caveat: data available via this url has been downloaded at an **earlier** and **unspecified** date!
+    geforce_url = get_geforce_backup_url()
 
     data = fetch_all_pages(is_slim_query=False, geforce_url=geforce_url)
     save_to_disk(data, "data_slim.json", verbose=True)
